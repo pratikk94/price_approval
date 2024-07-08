742a1f1a3f7ee25ac46eb233c32121eeff79b60a
Haneef <mshiraz@innominds.com>
2024-07-08 16:02:32 +0530

    added the store procedure for trancastion history and made some changes in the api

--- website/backend_mvc/controllers/requestHistoryController.js
+++ website/backend_mvc/controllers/requestHistoryController.js
@@ -8,14 +8,7 @@ async function fetchTransactionsByRequestId(req, res) {
       requestId
     );
 
-    // Check if any transaction has status == 1 and modify the message
-    const accepted = transactions.some((t) => t.status === 1);
-
-    const message = accepted
-      ? "Request accepted"
-      : "Transactions fetched successfully";
-
-    res.json({ message, data: transactions });
+    res.json({ data: transactions });
   } catch (err) {
     console.error("Error fetching transactions", err);
     res.status(500).send("Failed to fetch transactions");
